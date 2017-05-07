2b2238d57f69b8a11eb0501ee0597c2f0ad5cc45
Fant <fant7439@gmail.com>
2017-05-07 14:31:00 +0800

    Update Email_script.py

--- Email_script.py
+++ Email_script.py
@@ -1,16 +1,15 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import smtplib
-import Get_weather
 from email.mime.text import MIMEText
 def Post_Email(content):
 	mail_host = 'smtp.qq.com'
-    # Get_weather.Get_weather()
 	mail_user = '2413633136@qq.com'
 	mail_pass = 'xxx'
 	sender = '2413633136@qq.com'
 	receivers = ['2413633136@qq.com']
 	message = MIMEText(content, 'plain', 'utf-8')
-	message['Subject'] = 'The New Weather of 儋州'
+	message['Subject'] = "From fant's Email_Script"
 	message['From'] = sender
 	message['To'] = receivers[0]
 	try:
@@ -19,7 +18,6 @@ def Post_Email(content):
 		smtpObj.login(mail_user, mail_pass)
 		smtpObj.sendmail(sender, receivers, message.as_string())
 		smtpObj.quit()
-		#print('success')
 	except smtplib.SMTPException as e:
 		print('error', e)
 
